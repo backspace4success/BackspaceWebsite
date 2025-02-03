f006bd8236a45f0a1c2a4b984046f1f3bc8388d1
Backspace <165302977+backspace4success@users.noreply.github.com>
2025-02-03 13:31:22 +0200

    Update Testimonials.tsx

--- src/pages/home/Testimonials.tsx
+++ src/pages/home/Testimonials.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TestimonialsSlider from '../../components/TestimonialsSlider';
-import { testimonials } from '../../data/testimonials';
+import { testimonialsImages } from '../../data/testimonials';
 
 function Testimonials() {
   return (
@@ -8,10 +8,10 @@ function Testimonials() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-16">
           <h2 className="text-3xl font-bold text-black mb-4">Student Success Stories</h2>
-          <p className="text-xl text-black/70">Hear from our graduates who transformed their careers</p>
+          <p className="text-xl text-black/70">See the journeys of our successful graduates</p>
         </div>
         <div className="max-w-4xl mx-auto">
-          <TestimonialsSlider testimonials={testimonials} />
+          <TestimonialsSlider images={testimonialsImages} />
         </div>
       </div>
     </div>
